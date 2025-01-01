b8218aa7c352823c4390b9b495df4b052f54b253
Malcolm Anderson <malcolminyo@gmail.com>
2024-12-31 18:30:45 -0800

    Optimize exclamation bubbles, close #6

--- ui/exclamations/exclamation_bubble.gd
+++ ui/exclamations/exclamation_bubble.gd
@@ -1,16 +1,64 @@
-class_name ExclamationBubble
-extends AnimatedSprite2D
+extends Node2D
 
-static var instance: ExclamationBubble
+var magnitude: float = 0.0
+var remaining: float = 0.0
 
-func _enter_tree():
-	instance = self
+@onready var inner_container: Node2D = $InnerContainer
+@onready var bubble_display: Sprite2D = %BubbleDisplay
 
+const BUILTIN_BUBBLES = {
+	"objection": preload("res://ui/exclamations/objection.png"),
+	"holdit": preload("res://ui/exclamations/holdit.png"),
+	"takethat": preload("res://ui/exclamations/takethat.png")
+}
+
+enum { STATE_WAITING, STATE_SHAKING, STATE_STILL }
+var _state: int = STATE_WAITING
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	ScriptManager.register_handler("bubble", _handle_bubble)
 
 func _handle_bubble(args: Dictionary):
-	play_exclamation(args["type"])
+	var type = args.get("type", "objection")
+	magnitude = float(args.get("magnitude", "4.0"))
 
-func play_exclamation(type: String):
-	play(type)
+	# Load texture into sprite
+	if BUILTIN_BUBBLES.has(type):
+		var texture = BUILTIN_BUBBLES[type]
+		bubble_display.texture = texture
+	else:
+		Utils.print_error("Bubble type \"%s\" does not exist (must be \"objection\", \"holdit\", or \"takethat\")")
+		return
+
+	_state = STATE_SHAKING
+	remaining = 0.5
+
+func calculate_shake() -> Vector2:
+	var angle = randf_range(0, TAU)
+	return Vector2(int(cos(angle) * magnitude), int(sin(angle) * magnitude))
+
+func _process(delta: float):
+	match _state:
+		STATE_WAITING:
+			visible = false
+			return
+		STATE_SHAKING:
+			visible = true
+			remaining -= delta
+			var offset := Vector2.ZERO
+			if remaining > 0:
+				offset = calculate_shake()
+			else:
+				remaining = 0.5
+				_state = STATE_STILL
+			inner_container.position = offset
+			return
+		STATE_STILL:
+			visible = true
+			remaining -= delta
+			if remaining <= 0.0:
+				visible = false
+				_state = STATE_WAITING
+
+
