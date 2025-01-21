f05a1122cacbd6e8ac996e648b2344221fcb7d00
Malcolm Anderson <malcolminyo@gmail.com>
2025-01-20 18:14:33 -0800

    Add commands for Mood Matrix noise

--- ui/mood_matrix/noise/mm_noise.gd
+++ ui/mood_matrix/noise/mm_noise.gd
@@ -8,9 +8,78 @@ const VALID_NOISE_TYPES = ["100_0", "100_lp", "100_m", "100_s", "m_0", "m_100", 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ScriptManager.register_handler("mood_matrix.noise.animate", _handle_mood_matrix_noise_animate)
+	ScriptManager.register_handler("mood_matrix.noise.set_visible", _handle_mood_matrix_noise_set_visible)
+
+	ScriptManager.register_handler("mood_matrix.noise.set_label", _handle_mood_matrix_noise_set_label)
+
+	ScriptManager.register_handler("mood_matrix.noise.animate_in", _handle_mood_matrix_noise_animate_in)
+	ScriptManager.register_handler("mood_matrix.noise.animate_out", _handle_mood_matrix_noise_animate_out)
+
+	ScriptManager.register_handler("mood_matrix.noise.set_level", _handle_mood_matrix_noise_set_level)
+	ScriptManager.register_handler("mood_matrix.noise.animate_level", _handle_mood_matrix_noise_animate_level)
 	set_out()
-	await get_tree().create_timer(3.0).timeout
-	play_noise_animation(100, 20, "100_s")
+
+## Animates the Mood Matrix noise UI in, animates its value to a new value while
+## playing the corresponding sound clip, and then animates out.
+func _handle_mood_matrix_noise_animate(args: Dictionary):
+	# <mood_matrix.noise from="100" to="20" sound_code="100_s" />
+	var start_value: int = %NoiseLine.noise_level
+	if args.has("from"):
+		start_value = int(args["from"])
+	var end_value: int = int(args.get("to", "0"))
+	var sound_code: String = args.get("sound_code", "100_0")
+	await play_noise_animation(start_value, end_value, sound_code)
+
+## Sets the Mood Matrix noise UI in without any intro animation.
+func _handle_mood_matrix_noise_set_visible(args: Dictionary):
+	# <mood_matrix.noise.set_visible value="true" noise_value="100" />
+	var now_visible: bool = args.get("value", "true") == "true"
+	
+	if args.has("noise_level"):
+		var noise_level: int = int(args["noise_level"])
+		%PercentLabel.text = "%s%%" % noise_level
+
+	if now_visible:
+		set_in()
+	else:
+		set_out()
+
+## Sets the text for the Mood Matrix noise UI's "NOISE LEVEL" label.
+func _handle_mood_matrix_noise_set_label(args: Dictionary):
+	# <mood_matrix.noise.set_label text="NOISE LEVEL" />
+	var new_text: String = args.get("text", "NOISE LEVEL")
+	%NoiseLevelLabel.text = new_text
+
+## Simply animates in the Mood Matrix noise UI.
+func _handle_mood_matrix_noise_animate_in(_args: Dictionary):
+	await animate_in().finished
+
+## Simply animates out the Mood Matrix noise UI.
+func _handle_mood_matrix_noise_animate_out(_args: Dictionary):
+	await animate_out().finished
+
+## Sets the noise value on the Mood Matrix noise UI.
+func _handle_mood_matrix_noise_set_level(args: Dictionary):
+	if not args.has("level"):
+		Utils.print_error("mood_matrix.noise.set_level needs \"level\" argument")
+		return
+	var level: int = int(args["level"])
+	%PercentLabel.text = "%s%%" % level
+	%NoiseLine.set_noise_level(level)
+
+func _handle_mood_matrix_noise_animate_level(args: Dictionary):
+	# <mood_matrix.noise.animate_level from="100" to="0" />
+	var start_value: int = %NoiseLine.noise_level
+	if args.has("from"):
+		start_value = int(args["from"])
+	
+	if not args.has("to"):
+		Utils.print_error("mood_matrix.noise_animate_level needs \"to\" argument")
+		return
+	
+	var end_value: int = int(args["to"])
+	await animate_to_new(start_value, end_value).finished
 
 func play_noise_animation(start_value: int, end_value: int, sound_code: String):
 	if sound_code not in VALID_NOISE_TYPES:
@@ -19,6 +88,7 @@ func play_noise_animation(start_value: int, end_value: int, sound_code: String):
 		noise_sound_player.stream = load("res://ui/mood_matrix/sounds/noise/kokoro_noise_%s.wav" % sound_code)
 		noise_sound_player.play()
 	
+	%PercentLabel.text = "%s%%" % start_value
 	%NoiseLine.set_noise_level(start_value)
 	await animate_in().finished
 
@@ -43,13 +113,18 @@ func play_noise_animation(start_value: int, end_value: int, sound_code: String):
 		await animate_out().finished
 		return
 
-func _process(delta):
+func _process(_delta):
 	%PercentLabel.pivot_offset = %PercentLabel.size / 2.0
 
 func set_out():
 	%CenterContent.scale.y = 0.0
 	%CenterContent.modulate.a = 0.0
 	%BackgroundOverlay.modulate.a = 0.0
+
+func set_in():
+	%CenterContent.scale.y = 1.0
+	%CenterContent.modulate.a = 1.0
+	%BackgroundOverlay.modulate.a = 1.0
 
 var trans_tween: Tween = null
 func animate_in() -> Tween:	
