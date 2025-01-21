a4bf6a4eee64ece9832281f5ed88532fa9650411
Malcolm Anderson <malcolminyo@gmail.com>
2025-01-20 23:06:13 -0800

    Make some improvements to UI animation sound effects and command syntax

--- ui/mood_matrix/mood_matrix.gd
+++ ui/mood_matrix/mood_matrix.gd
@@ -6,19 +6,20 @@ extends Control
 @onready var _surprised_marker: MoodMatrixMarker = %Surprised
 
 func _ready():
-	ScriptManager.register_handler("mood_matrix.ui", _handle_mood_matrix_ui)
+	ScriptManager.register_handler("mood_matrix.bootup.animate_in", _handle_mood_matrix_bootup_animate_in)
+	ScriptManager.register_handler("mood_matrix.ui.animate_in", _handle_mood_matrix_ui_animate_in)
+
+	ScriptManager.register_handler("mood_matrix.ui.set_visible", _handle_mood_matrix_ui_set_visible)
+
 	ScriptManager.register_handler("mood_matrix.emotion", _handle_mood_matrix_emotion)
 
-	%Bootup.sound_should_be_played.connect(%BeginSound.play)
+	%Bootup.sound_should_be_played.connect(%BootupSound.play)
 	%Bootup.white_flash.connect(func():
 		var a = create_tween()
 		%WhiteFlash.visible = true
 		%WhiteFlash.color.a = 0.0
 		a.tween_property(%WhiteFlash, "color:a", 1.0, 0.09)
 	)
-
-func _handle_mood_matrix(args: Dictionary):
-	pass
 
 func set_markers_thinking():
 	_happy_marker.set_thinking()
@@ -44,7 +45,9 @@ func do_intro_pulse():
 	_angry_marker.do_intro_pulse_anim()
 	_surprised_marker.do_intro_pulse_anim()
 
-func animate_markers_in():
+func animate_markers_in(play_sound: bool = true):
+	if play_sound:
+		%RestartSound.play()
 	%WhiteFlash.visible = false
 	%WhiteFlash.color.a = 0.0
 	$AnimationPlayer.play("markers_in")
@@ -56,14 +59,20 @@ func animate_bootup():
 	await bootup_anim.animation_finished
 	bootup_anim.play("RESET")
 
-func _handle_mood_matrix_ui(args: Dictionary):
-	## Options:
-	## - animate="bootup"
-	var animate = args.get("animate", "in")
-	if animate == "bootup":
-		await animate_bootup()
-	if animate == "emotions_in":
-		await animate_markers_in()
+func _handle_mood_matrix_bootup_animate_in(_args: Dictionary):
+	await animate_bootup()
+
+func _handle_mood_matrix_ui_animate_in(args: Dictionary):
+	var play_sound: bool = args.get("play_sound", "true") == "true"
+	await animate_markers_in(play_sound)
+
+func _handle_mood_matrix_ui_set_visible(args: Dictionary):
+	var value: bool = args.get("value", "true") == "true"
+	if value:
+		$AnimationPlayer.assigned_animation = "markers_in"
+		$AnimationPlayer.seek($AnimationPlayer.current_animation_length, true, true)
+	else:
+		$AnimationPlayer.play("RESET")
 
 func _handle_mood_matrix_emotion(args: Dictionary):
 	# <mood_matrix.emotion type="happy" intensity="3" />
